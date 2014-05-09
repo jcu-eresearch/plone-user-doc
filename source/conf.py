788fac0a90ab272c0ad9c16e478e58f50fdc7819
David\ Beitey <david@davidjb.com>
2014-05-09 14:47:26 +1000

    Remove old theme

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,6 @@
 
 import sys, os
 import datetime
-import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
